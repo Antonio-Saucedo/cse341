749d1c92a48a5fee10e2bef5e9f022fe6707055c
Antonio-Saucedo <antoniojesus@byui.edu>
2023-05-10 19:10:40 -0600

    frontend minor fix

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-with, Content-TypeError, Accept, Z-key'
+      'Origin, X-Requested-With, Content-Type, Accept, Z-key'
     );
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
