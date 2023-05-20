6a60ce68c1e5e449537d84fe4bc31d10730d8a49
Antonio-Saucedo <antoniojesus@byui.edu>
2023-05-20 00:58:37 -0600

    Swagger fixes

--- controllers/pickaxes.js
+++ controllers/pickaxes.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 // const ObjectId = require('mongodb').ObjectId;
 
 const getAllPickaxeData = async (req, res) => {
-  const result = await mongodb.getDb().db('valheim').collection('pickaxes').find();
+  const result = await mongodb.getDb().db('valheim').collection('pickaxe').find();
   result.toArray().then((lists) => {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
